93e043255ef703543f328982c67476870b839ccc
QuercusFelis <37717887+QuercusFelis@users.noreply.github.com>
2019-09-20 07:11:40 +0100

    bug fixes

--- THModule.py
+++ THModule.py
@@ -1,14 +1,14 @@
 import sys
 import Adafruit_DHT
 
-sensor = Adafruit_DHT.AM2303
-pin = 15
+sensor = Adafruit_DHT.AM2302
+pin = 4
 
 humidity, temp = Adafruit_DHT.read_retry(sensor, pin) #read temp
-temp = temp * 9/5.0 + 32 #convert celcius to fahrenheit
 
-if humidity in not None and temp is not None:
-    print('temp: {0:0.1f}f  humidity: {1:0.1f}%')
+if humidity is not None and temp is not None:
+    temp = temp * 9/5.0 + 32 #convert celcius to fahrenheit
+    print('temp: {0:0.1f}f  humidity: {1:0.1f}%'.format(temp, humidity))
 else:
-    printerr('Failed to obtain am2303 reading')
+    sys.stderr.write('Failed to obtain am2303 reading\n')
     sys.exit(1)
