41c152c5fe5eb754dfdce977eae6ed117c864e7e
QuercusFelis <quercusfelis@gmail.com>
2020-01-31 15:55:48 -0700

    make code callable as functions for flexibility & custom scripts

--- THModule.py
+++ THModule.py
@@ -2,28 +2,31 @@ import sys
 import Adafruit_DHT
 import configparser
 
-confParser = configparser.RawConfigParser()
-confParser.read(r'growbot.conf')
+def readTHModule()
+    confParser = configparser.RawConfigParser()
+    confParser.read(r'growbot.conf')
 
-sensorModel = confParser.get('growbot-conf', 'temp_humidity_sensor')
-pin = confParser.getint('growbot-conf', 'temp_humidity_pin')
-sensor = None
-if sensorModel == 'AM2302': 
-        sensor = Adafruit_DHT.AM2302
-elif sensorModel == 'DHT22':
-        sensor = Adafruit_DHT.DHT22
-elif sensorModel == 'DHT11':
-        sensor = Adafruit_DHT.DHT11
-else:
-        sys.stderr.write('growbot-x: INVALID SENSOR TYPE IN CONFIG - i.e. AM2303, DHT22, DHT11' + sensorModel)
-        exit(1)
+    sensorModel = confParser.get('growbot-conf', 'temp_humidity_sensor')
+    pin = confParser.getint('growbot-conf', 'temp_humidity_pin')
+    sensor = None
+    if sensorModel == 'AM2302': 
+            sensor = Adafruit_DHT.AM2302
+    elif sensorModel == 'DHT22':
+            sensor = Adafruit_DHT.DHT22
+    elif sensorModel == 'DHT11':
+            sensor = Adafruit_DHT.DHT11
+    else:
+            sys.stderr.write('growbot-x: INVALID SENSOR TYPE IN CONFIG - i.e. AM2303, DHT22, DHT11' + sensorModel)
+            exit(1)
 
-humidity, temp = Adafruit_DHT.read_retry(sensor, pin) #read temp
+    humidity, temp = Adafruit_DHT.read_retry(sensor, pin) #read temp
 
-if humidity is not None and temp is not None:
-    temp = temp * 9/5.0 + 32 #convert celcius to fahrenheit
-    print('temp: {0:0.1f}f  humidity: {1:0.1f}%'.format(temp, humidity))
-    exit(0)
-else:
-    sys.stderr.write('growbot-x: Failed to obtain temperature & humidity reading\n')
-    exit(2)
+    if humidity is not None and temp is not None:
+        temp = temp * 9/5.0 + 32 #convert celcius to fahrenheit
+        return('temp: {0:0.1f}f  humidity: {1:0.1f}%'.format(temp, humidity))
+    else:
+        sys.stderr.write('growbot-x: Failed to obtain temperature & humidity reading\n')
+        exit(2)
+
+print(readTHModule())
+exit(0)
