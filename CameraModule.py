25aaba761f2962e073b5497e42cc11f48bd49725
quercusfelis <37717887+QuercusFelis@users.noreply.github.com>
2023-02-11 17:01:09 -0700

    Tune camera parameters

--- CameraModule.py
+++ CameraModule.py
@@ -7,8 +7,8 @@ def photo():
         camera.rotation = -90
         camera.resolution = [2592,1944]
         camera.awb_mode = 'off'
-        camera.awb_gains = (0.70,0.55)
-        camera.contrast = -7 
+        camera.awb_gains = (0.75,0.52)
+        camera.saturation = 5 
         camera.iso = 100
         camera.shutter_speed = 1400
         camera.image_denoise = False
